a9dcbd2570d38f531de3a7b4e115559f0a4a3f2c
Brandon <74426342+Brandonbr1@users.noreply.github.com>
2021-10-18 16:47:00 -0700

    Add files via upload

---
+++ src/main/java/me/brandon/dreamaxolotl/recreation/dreamaxolotlrecreation/Dreamaxolotlrecreation.java
@@ -0,0 +1,17 @@
+package me.brandon.dreamaxolotl.recreation.dreamaxolotlrecreation;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Dreamaxolotlrecreation extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/brandon/dreamaxolotl/recreation/dreamaxolotlrecreation/Drown.java
@@ -0,0 +1,35 @@
+package me.brandon.dreamaxolotl.recreation.dreamaxolotlrecreation;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Effect;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.Vector;
+import org.bukkit.entity.Entity.Spigot;
+
+public class Drown implements Listener {
+    private final String prefix = (Object) ChatColor.BOLD + "SkyShock" + ChatColor.DARK_GRAY.toString() + (Object)ChatColor.BOLD + ">>" + " ";
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+        player.setVelocity(new Vector(player.getVelocity().getX(), player.getVelocity().getY() / 0.01, player.getVelocity().getZ()));
+        player.setFallDistance(player.getFallDistance() / 3.0f); //Please ignore the Velocity part, I know its wrong cause I tested it and it doesn't work. If you guys want to as well explain how to fix this feel free to do so, but for the most part I'll try figuring out how to fix it later...
+        player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 3));
+        player.sendMessage(String.valueOf(this.prefix) + (Object)ChatColor.DARK_AQUA + "You have entered a space realm...");
+        player.addPotionEffect(new PotionEffect(PotionEffectType.POISON, Integer.MAX_VALUE, 1));
+        player.setMaximumAir(300); // setMaximumAir(int 10); a.getKey().setRemainingAir(a.getValue() / 100 * 3); may be better
+
+    }
+
+}
